42a4c749f6c34c208d20998d1e2b600a7318e4c8
razevedo1994 <razevedo.contato@gmail.com>
2022-04-24 12:26:05 -0300

    add: tuples

---
+++ tuples/portfolio.py
@@ -0,0 +1,24 @@
+'''
+This program shows how you might read a file containing columns of data
+separated by commas.
+'''
+ # File containing lines of the form "name,shares,price"
+
+
+filename = 'tuples/portfolio.csv'
+
+portfolio = []
+with open(filename) as file:
+    for line in file:
+        row = line.split(',')
+        name = row[0]
+        shares = int(row[1])
+        price = float(row[2])
+        holding = (name, shares, price)
+        portfolio.append(holding)
+
+
+print(portfolio[0])
+print(portfolio[1])
+print(portfolio[1][1])
+print(portfolio[1][2])
